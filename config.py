511b0b670b81144bbfc7d8539e7166f3a8c6009e
Swaraag <swaraag.sistla@gmail.com>
2026-07-08 23:42:20 -0700

    feat: update requirements.txt; update config.py with API information

--- config.py
+++ config.py
@@ -1,6 +1,18 @@
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+load_dotenv()
 
+# ===============================
+# FILE PATHS
+# ===============================
 REPO_ROOT = Path(__file__).parent
 DATA_ROOT = REPO_ROOT / "data"
 PARTICIPANTS_FILE_NAME = "TDBRAIN_participants_V3.xlsx"
 PROMPTS_ROOT = REPO_ROOT / "prompts"
+
+# ===============================
+# API INFORMATION
+# ===============================
+OPENROUTER_API_KEY = os.getenv("OPENROUTER_API_KEY")
+OPENROUTER_BASE_URL = "https://openrouter.ai/api/v1"
